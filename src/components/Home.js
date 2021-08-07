cd9e3a3908d07437ee517f619a31851cc10682ba
Erik Z <erikzhou7@gmail.com>
2021-08-06 21:46:02 -0400

    Styled Home Page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,17 @@
 import React from "react"
-
+import image from "../background.jpg"
 const Home = () =>{
     return (
-        <div> Home </div>
+        <main>
+            <img 
+                src={image} 
+                alt="Gradient Background"
+                className="absolute object-cover w-full h-full"
+            />
+            <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
+                <h1 className="text-4xl text-blue-200 font-bold serif leading-none lg:leading-snug home-name"> Hello, I am Erik</h1>
+            </section>
+        </main>
     )
 }
 
