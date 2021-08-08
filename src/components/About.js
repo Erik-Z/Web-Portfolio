c77af063eb00efe360ba228a25752ea89dd2ee13
Erik Z <erikzhou7@gmail.com>
2021-08-08 01:25:17 -0400

    Changed Styling

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,21 @@ import React from "react"
 
 const About = () => {
     return(
-        <div> About </div>
+        <main className="bg-blue-200 min-h-screen p-12">
+            <section className="container mx-auto">
+                <section className="grid grid-cols-1  gap-8">
+                    <article className="relative rounded-lg shadow-xl bg-blue-100 p-16">
+                        <p>
+                            Hi, my name is Erik, I graduated with a Computer Science undergraduate degree from Stony Brook University. I have most experience with web development, but can also program mobile applications and do game development. The languages I am most proficient in are JavaScript, Python, and Java. I am an active learner so picking up new languages and technologies is not a problem for me. I have done enough projects to be able to approach a wide range of problems in a pragmatic and efficient way.
+                        </p>
+
+                        <p>
+                            
+                        </p>
+                    </article>
+                </section>
+            </section>
+        </main>
     )   
 }
 
